8e4f7db25ac28a8466482d563eab05905d402f30
ichinosee <67300586+ichinosee@users.noreply.github.com>
2021-01-21 17:49:36 +0900

    Add files via upload

---
+++ StarDAO.java
@@ -0,0 +1,30 @@
+package la.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.sun.jdi.connect.spi.Connection;
+
+import la.bean.StarBean;
+
+public class StarDAO {
+	private Connection con;
+
+	public StarDAO() throws DAOException{
+		getConnection();
+	}
+
+	public String SveStar(StarBean star)
+	throws DAOException{
+		if(con == null)
+			getConnection();
+
+		PreparedStatement st = null;
+		ResultSet rs = null;
+	}
+
+	private void getConnection() throws DAOException{
+
+	}
+
+}
