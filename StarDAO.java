13b6bda1bf9970e42b2aedc13c96202556709581
ichinosee <67300586+ichinosee@users.noreply.github.com>
2021-01-26 00:50:15 +0900

    Update StarDAO.java

--- StarDAO.java
+++ StarDAO.java
@@ -1,14 +1,16 @@
+//作成途中
+
 package la.dao;
 
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import com.sun.jdi.connect.spi.Connection;
 
 import la.bean.StarBean;
 
-
-//作成途中
 public class StarDAO {
 	private Connection con;
 
@@ -23,10 +25,54 @@ public class StarDAO {
 
 		PreparedStatement st = null;
 		ResultSet rs = null;
+
+		try {
+			int code = 0;
+			String sql = "SELECT nextval('customer_code_seq')";
+			st = con.preparedStatement(sql);
+			rs = st.executeQuery();
+			if(rs.next()) {
+				code = rs.getInt(1);
+			}
+
+			rs.close();
+			st.close();
+
+			
+			sql = "INSERT INTO star VALUES(?, ?)";
+			st = con.preparedStatement(sql);
+
+			st.setInt(1, code);
+			st.setString(2, star.star());
+
+			st.executeUpdate();
+			st.close();
+
+		}
 	}
 
-	private void getConnection() throws DAOException{
 
+	private void getConnection() throws DAOException{
+		try {
+			Class.forName("org.postgresql.Driver");
+
+			String url = "jdbc:postgresql:nose";
+			String user = "student";
+			String pass = "himitu";
+
+			con = DriverManager.getConnection(url, user, pass);
+		}catch(Exception e) {
+			e.printStackTrace();
+			throw new DAOException("接続に失敗しました");
+		}
+
+	}
+
+	private void close() throws SQLException {
+		if(con != null) {
+			con.close();
+			con = null;
+		}
 	}
 
 }
